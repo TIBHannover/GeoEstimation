617df48123eb345aea27424492ba174a990974e1
Eric Mueller <eric.mueller@tib.eu>
2018-09-10 20:09:36 +0200

    added cpu mode

--- inference.py
+++ inference.py
@@ -1,7 +1,8 @@
 import argparse
-import logging
 import os
 import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppresses unnecessarily excessive console output
+import tensorflow as tf
 
 # own imports
 import scene_classification
@@ -13,7 +14,6 @@ cur_path = os.path.abspath(os.path.dirname(__file__))
 
 def parse_args():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
     parser.add_argument('-i', '--image', type=str, required=True, help='path to image file')
     parser.add_argument(
         '-m',
@@ -22,6 +22,7 @@ def parse_args():
         default='ISN',
         choices=['ISN', 'base_L', 'base_M'],
         help='Choose from [ISN, base_L, base_M]')
+    parser.add_argument('-c', '--cpu', action='store_true', help='use cpu')
     args = parser.parse_args()
     return args
 
@@ -30,32 +31,34 @@ def main():
     # load arguments
     args = parse_args()
 
-    # define logging level and format
-    level = logging.ERROR
-    if args.verbose:
-        level = logging.DEBUG
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=level)
+    # check if gpu is available
+    if not tf.test.is_gpu_available():
+        print('No GPU available. Using CPU instead ... ')
+        args.cpu = True
 
     # init models
     if args.model == 'ISN':
         # init model for scene_classification
-        sc = scene_classification.SceneClassifier()
+        sc = scene_classification.SceneClassifier(use_cpu=args.cpu)
 
         # init models for geolocation estimation
         # init ISN for concept 'indoor'
         ge_indoor = geo_estimation.GeoEstimator(
-            os.path.join(cur_path, 'models', 'ISN_M_indoor', 'model.ckpt'), scope='indoor')
+            os.path.join(cur_path, 'models', 'ISN_M_indoor', 'model.ckpt'), scope='indoor', use_cpu=args.cpu)
         # init ISN for concept 'natural'
         ge_natural = geo_estimation.GeoEstimator(
-            os.path.join(cur_path, 'models', 'ISN_M_natural', 'model.ckpt'), scope='natural')
+            os.path.join(cur_path, 'models', 'ISN_M_natural', 'model.ckpt'), scope='natural', use_cpu=args.cpu)
         # init ISN for concept 'urban'
         ge_urban = geo_estimation.GeoEstimator(
-            os.path.join(cur_path, 'models', 'ISN_M_urban', 'model.ckpt'), scope='urban')
+            os.path.join(cur_path, 'models', 'ISN_M_urban', 'model.ckpt'), scope='urban', use_cpu=args.cpu)
     elif args.model == 'base_L':
         ge_base = geo_estimation.GeoEstimator(
-            os.path.join(cur_path, 'models', 'base_L_m', 'model.ckpt'), scope='base_L_m')
+            os.path.join(cur_path, 'models', 'base_L_m', 'model.ckpt'), scope='base_L_m', use_cpu=args.cpu)
     elif args.model == 'base_M':
-        ge_base = geo_estimation.GeoEstimator(os.path.join(cur_path, 'models', 'base_M', 'model.ckpt'), scope='base_M')
+        ge_base = geo_estimation.GeoEstimator(
+            os.path.join(cur_path, 'models', 'base_M', 'model.ckpt'), scope='base_M', use_cpu=args.cpu)
+
+    print('Processing: {}'.format(args.image))
 
     # predict scene label
     if args.model == 'ISN':
